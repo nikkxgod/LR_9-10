0f1775a3a50044ed5deb85f3ebdfe7d21227c66f
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-11-17 10:30:05 +0300

    Add files via upload

---
+++ 10.1.pas
@@ -0,0 +1,17 @@
+﻿var A:array[1..6,1..8] of integer;i,j:integer; B:array[1..6] of integer;
+  begin
+    for i:=1 to 6 do begin
+      for j:=1 to 8 do A[i,j]:=random(10)-5;
+    end;
+    for i:=1 to 6 do begin
+      for j:=1 to 8 do begin
+        if abs(A[i,j])>4 then begin
+        B[i]:=A[i,j];
+        break;
+        end
+        else B[i]:=0;
+      end;
+    end;
+    writeln('Исходный массив A: ',A);
+    writeln('Новый массив: ',B);
+end.
